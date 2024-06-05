c4f29b00b300f648fb7fa6790b9c0f54468b16ff
djinnalexio <pakueamb@gmail.com>
2024-06-05 14:55:32 +0100

    refactor(settings): :lipstick: reduced prefs window size

--- eye-on-cursor@djinnalexio.github.io/prefs.js
+++ eye-on-cursor@djinnalexio.github.io/prefs.js
@@ -28,9 +28,8 @@ import {TrackerPage} from './settings/trackerSettings.js';
 
 export default class EyeOnCursorPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
-        window.search_enabled = true;
         window.default_width = 465;
-        window.default_height = 825;
+        window.default_height = 700;
 
         window.add(new EyePage(this));
         window.add(new TrackerPage(this));
