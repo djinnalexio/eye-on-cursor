c1d91d363fee703617d744bb1d387350a9d248fa
djinnalexio <pakueamb@gmail.com>
2025-02-14 23:31:41 +0100

    chore: :necktie: updated copyright

--- eye-on-cursor/prefs.js
+++ eye-on-cursor/prefs.js
@@ -2,7 +2,7 @@
  *
  * This file is part of the Eye on Cursor GNOME Shell extension (eye-on-cursor@djinnalexio.github.io).
  *
- * Copyright (C) 2024 djinnalexio
+ * Copyright (C) 2024-2025 djinnalexio
  *
  * This extension is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
